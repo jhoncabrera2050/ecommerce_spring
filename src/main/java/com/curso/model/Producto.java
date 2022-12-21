78e1220843d30fe51e87775169eb835541e03102
Jhon Cabrera <jhoncabrera2050@gmail.com>
2022-12-21 18:33:07 -0500

    creacion de la escructura y las clases

---
+++ src/main/java/com/curso/model/Producto.java
@@ -0,0 +1,58 @@
+package com.curso.model;
+
+public class Producto {
+	//creacion de atributos 
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	private String imagen;
+	private Double precio;
+	private int cantidad;
+	//creacion de un constructor vacio
+	public Producto() {
+		
+	}
+	//constructor con parametros
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
+				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
+	}
+		
+}
